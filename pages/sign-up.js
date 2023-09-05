b00ead3e6538ca45b6333708eac3b7b2d8bc8801
rhodorace <rhodorace@gmail.com>
2023-09-05 21:58:53 +0200

    Adjust signup page

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,69 +1,126 @@
+import { Typography, Button } from "@material-tailwind/react";
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Input,
-  Checkbox,
-  Button,
-  Typography,
-} from "@material-tailwind/react";
-import Footer from "@/components/footer";  
+  MapPinIcon,
+  BriefcaseIcon,
+  BuildingLibraryIcon,
+} from "@heroicons/react/24/solid";
+import Footer from "@/components/footer";
 
 export default function SignUp() {
   return (
     <>
-      <img
-        src="/imgages/background-2.jpg"
-        className="absolute inset-0 z-0 h-full w-full object-cover"
-      />
-      <div className="absolute inset-0 z-0 h-full w-full bg-black/50" />
-      <div className="container mx-auto p-4">
-        <Card className="absolute top-2/4 left-2/4 w-full max-w-[24rem] -translate-y-2/4 -translate-x-2/4">
-          <CardHeader
-            variant="gradient"
-            color="blue"
-            className="mb-4 grid h-28 place-items-center"
-          >
-            <Typography variant="h3" color="white">
-              Sign Up
-            </Typography>
-          </CardHeader>
-          <CardBody className="flex flex-col gap-4">
-            <Input variant="standard" label="Name" size="lg" />
-            <Input variant="standard" type="email" label="Email" size="lg" />
-            <Input
-              variant="standard"
-              type="password"
-              label="Password"
-              size="lg"
-            />
-            <div className="-ml-2.5">
-              <Checkbox label="I agree the Terms and Conditions" />
-            </div>
-          </CardBody>
-          <CardFooter className="pt-0">
-            <Button variant="gradient" fullWidth>
-              Sign Up
-            </Button>
-            <Typography variant="small" className="mt-6 flex justify-center">
-              Already have an account?
-              {/* <Link to="/sign-in">
-                <Typography
-                  as="span"
-                  variant="small"
-                  color="blue"
-                  className="ml-1 font-bold"
-                >
-                  Sign in
+      <section className="relative block h-[50vh]">
+        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/images/mountain.jpg')] bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
+      </section>
+      <section className="relative bg-blue-gray-50/50 py-16 px-4">
+        <div className="container mx-auto">
+          <div className="relative mb-6 -mt-64 flex w-full min-w-0 flex-col break-words rounded-3xl bg-white shadow-xl shadow-gray-500/5">
+            <div className="px-6">
+              <div className="flex flex-wrap justify-center">
+                <div className="flex w-full justify-center px-4 lg:order-2 lg:w-3/12">
+                  <div className="relative">
+                    <div className="-mt-20 w-40">
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-10 flex w-full justify-center px-4 lg:order-3 lg:mt-0 lg:w-4/12 lg:justify-end lg:self-center">
+                  <Button className="bg-blue-400">Conntect</Button>
+                </div>
+                <div className="w-full px-4 lg:order-1 lg:w-4/12">
+                  <div className="flex justify-center py-4 pt-8 lg:pt-4">
+                    <div className="mr-4 p-3 text-center">
+                      <Typography
+                        variant="lead"
+                        color="blue-gray"
+                        className="font-bold uppercase"
+                      >
+                        22
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        className="font-normal text-blue-gray-500"
+                      >
+                        Friends
+                      </Typography>
+                    </div>
+                    <div className="mr-4 p-3 text-center">
+                      <Typography
+                        variant="lead"
+                        color="blue-gray"
+                        className="font-bold uppercase"
+                      >
+                        10
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        className="font-normal text-blue-gray-500"
+                      >
+                        Photos
+                      </Typography>
+                    </div>
+                    <div className="p-3 text-center lg:mr-4">
+                      <Typography
+                        variant="lead"
+                        color="blue-gray"
+                        className="font-bold uppercase"
+                      >
+                        89
+                      </Typography>
+                      <Typography
+                        variant="small"
+                        className="font-normal text-blue-gray-500"
+                      >
+                        Comments
+                      </Typography>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="my-8 text-center">
+                <Typography variant="h2" color="blue-gray" className="mb-2">
+                  Jenna Stones
                 </Typography>
-              </Link> */}
-            </Typography>
-          </CardFooter>
-        </Card>
-      </div>
-      <div className="container absolute bottom-6 left-2/4 z-10 mx-auto -translate-x-2/4 text-white">
-        <Footer  />
+                <div className="mb-16 flex items-center justify-center gap-2">
+                  <MapPinIcon className="-mt-px h-4 w-4 text-blue-gray-700" />
+                  <Typography className="font-medium text-blue-gray-700">
+                    Los Angeles, California
+                  </Typography>
+                </div>
+                <div className="mb-2 flex items-center justify-center gap-2">
+                  <BriefcaseIcon className="-mt-px h-4 w-4 text-blue-gray-700" />
+                  <Typography className="font-medium text-blue-gray-700">
+                    Solution Manager - Creative Tim Officer
+                  </Typography>
+                </div>
+                <div className="mb-2 flex items-center justify-center gap-2">
+                  <BuildingLibraryIcon className="-mt-px h-4 w-4 text-blue-gray-700" />
+                  <Typography className="font-medium text-blue-gray-700">
+                    University of Computer Science
+                  </Typography>
+                </div>
+              </div>
+
+              <div className="mb-10 border-t border-blue-gray-50 py-6 text-center">
+                <div className="mt-2 flex flex-wrap justify-center">
+                  <div className="flex w-full flex-col items-center px-4 lg:w-9/12">
+                    <Typography className="mb-8 font-normal text-blue-gray-500">
+                      An artist of considerable range, Jenna the name taken by
+                      Melbourne-raised, Brooklyn-based Nick Murphy writes,
+                      performs and records all of his own music, giving it a
+                      warm, intimate feel with a solid groove structure. An
+                      artist of considerable range.
+                    </Typography>
+                    <Button variant="text">Show more</Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="bg-blue-gray-50/50">
+        <Footer />
       </div>
     </>
   );
