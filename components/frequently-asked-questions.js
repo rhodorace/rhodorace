2cf59fb9c79655f92d4f8633f2c67a909db5e4e4
rhodorace <rhodorace@gmail.com>
2023-09-16 08:15:03 +0200

    Added content to frequently asked questions

--- components/frequently-asked-questions.js
+++ components/frequently-asked-questions.js
@@ -12,35 +12,33 @@ export function FrequentlyAskedQuestions() {
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
   return (
-   <section className="relative bg-gray-50 py-24 px-4">
-   <PageTitle heading="Често задавани въпроси"/>
+    <section className="relative bg-gray-50 py-24 px-4">
+      <PageTitle heading="Често задавани въпроси" />
       <div className="container mx-auto">
         <Accordion open={open === 1}>
-          <AccordionHeader onClick={() => handleOpen(1)}>What is Material Tailwind?</AccordionHeader>
+          <AccordionHeader onClick={() => handleOpen(1)}>Какъв е маршрутът на маратона в Родопите?</AccordionHeader>
           <AccordionBody>
-            We&apos;re not always in the position that we want to be at. We&apos;re constantly
-            growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-            ourselves and actualize our dreams.
+            Маратонът в Родопите предлага няколко различни маршрута за участниците, включително къс маратон, полумаратон и маршрут за разходки.
+            Точните маршрути се обявяват преди събитието и могат да бъдат намерени на нашата уеб страница и в информационния пакет.
           </AccordionBody>
         </Accordion>
         <Accordion open={open === 2}>
           <AccordionHeader onClick={() => handleOpen(2)}>
-            How to use Material Tailwind?
+            Каква е цената за участие и как мога да се регистрирам?
           </AccordionHeader>
           <AccordionBody>
-            We&apos;re not always in the position that we want to be at. We&apos;re constantly
-            growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-            ourselves and actualize our dreams.
+            Цените за участие в маратона в Родопите варират в зависимост от избрания маршрут и категорията на участник (възрастова група и други фактори).
+            За актуална информация относно цените и начина на регистрация, моля, посетете нашата уеб страница и следвайте инструкциите за регистрация.
           </AccordionBody>
         </Accordion>
         <Accordion open={open === 3}>
           <AccordionHeader onClick={() => handleOpen(3)}>
-            What can I do with Material Tailwind?
+            Има ли настаняване и транспорт за участниците извън Момчилград?
           </AccordionHeader>
           <AccordionBody>
-            We&apos;re not always in the position that we want to be at. We&apos;re constantly
-            growing. We&apos;re constantly making mistakes. We&apos;re constantly trying to express
-            ourselves and actualize our dreams.
+            Да, организираме настаняване и предоставяме информация за транспорт за участниците, които идват извън Момчилград.
+            Можете да се свържете с нашия екип за организация на събитието,
+            за да научите повече подробности и да резервирате настаняване или се консултирате относно транспортните опции.
           </AccordionBody>
         </Accordion>
       </div>
