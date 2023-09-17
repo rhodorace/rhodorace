1538f20b75a8a3f7e3aaacdf45d22a75d3862469
rhodorace <rhodorace@gmail.com>
2023-09-17 19:45:50 +0200

    Adjust header

--- components/video-heading.js
+++ components/video-heading.js
@@ -18,20 +18,20 @@ export default function VideoHeading() {
                     <div>
                         <div className="rhodo-stripe blue grid gap-2 grid-cols-2">
                             <div className="text-right">
-                                <h1 class="mb-4 mt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">RHODO RACE</h1>
+                                <h1 class="mb-4 mt-4 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">RHODO&nbsp;RACE</h1>
                             </div>
 
-                            <div>
-                                <h1 class="mb-4 mt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">21 APRIL</h1>
+                            <div className="lg:text-left sm:text-right">
+                                <h1 class="mb-4 mt-4 font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">21 APRIL</h1>
                             </div>
                         </div>
                         <div className="rhodo-stripe green grid gap-2 grid-cols-2">
                             <div className="text-right">
-                                <h1 class="mb-4 mt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">MOMCHILGRAD</h1>
+                                <h1 class="mb-4 mt-4  font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">MOMCHILGRAD</h1>
                             </div>
 
-                            <div className="text-right lg:text-left">
-                                <h1 class="mb-4 mt-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">2024</h1>
+                            <div className="lg:text-left sm:text-right">
+                                <h1 class="mb-4 mt-4  font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl text-white">2024</h1>
                             </div>
                         </div>
                     </div>
