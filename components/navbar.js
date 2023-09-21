b07b523ab2967bed3bd17b0015507bb257881768
rhodorace <rhodorace@gmail.com>
2023-09-21 20:21:24 +0200

    Refactor navbar

--- components/navbar.js
+++ components/navbar.js
@@ -22,137 +22,72 @@ export function Navbar() {
     );
   }, []);
 
-  const navList = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Link href="/sign-up" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          РЕГИСТРАЦИЯ
-        </Button>
+  let myList = [];
+  myList.push(<Link href="/sign-up" passHref>
+    <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
+      РЕГИСТРАЦИЯ
+    </Button>
+  </Link>);
+  myList.push(<Menu allowHover>
+    <MenuHandler>
+      <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
+        ТРАСЕТА
+      </Button>
+    </MenuHandler>
+    <MenuList>
+      <Link href="/track1" passHref>
+        <MenuItem>21 KM</MenuItem>
       </Link>
+      <Link href="/track2" passHref>
+        <MenuItem>42 KM</MenuItem>
+      </Link>
+    </MenuList>
+  </Menu>);
+  myList.push(<Link href="/news" passHref>
+    <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
+      НОВИНИ
+    </Button>
+  </Link>);
+  if (openNav === false) {
+    myList.push(<Image
+      priority
+      src={bgLogo}
+      width={200}
+      alt="RhodoRace Momchilgrad"
+    />);
+  }
+  myList.push(<Link href="/sponsors" passHref>
+    <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
+      СПОНСОРИ
+    </Button>
+  </Link>);
+  myList.push(<Menu allowHover>
+    <MenuHandler>
+      <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
+        ИСТОРИЯ
+      </Button>
+    </MenuHandler>
+    <MenuList>
+      <Link href="/history" passHref>
+        <MenuItem>2024</MenuItem>
+      </Link>
+    </MenuList>
+  </Menu>);
+  myList.push(<Link href="/contact" passHref>
+    <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
+      КОНТАКТИ
+    </Button>
+  </Link>);
 
-      <Menu allowHover>
-        <MenuHandler>
-          <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-            ТРАСЕТА
-          </Button>
-        </MenuHandler>
-        <MenuList>
-          <Link href="/track1" passHref>
-            <MenuItem>21 KM</MenuItem>
-          </Link>
-          <Link href="/track2" passHref>
-            <MenuItem>42 KM</MenuItem>
-          </Link>
-        </MenuList>
-      </Menu>
 
-      <Link href="/news" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          НОВИНИ
-        </Button>
-      </Link>
-
-      <Image
-        priority
-        src={bgLogo}
-        width={200}
-        alt="RhodoRace Momchilgrad"
-      />
-      {/* <Menu allowHover>
-        <MenuHandler>
-          <Button variant="gradient" className="lg:inline-block text-white" color="green">
-            ГАЛЕРИЯ
-          </Button>
-        </MenuHandler>
-        <MenuList>
-          <Link href="/image-gallery" passHref>
-            <MenuItem>2024</MenuItem>
-          </Link>
-        </MenuList>
-      </Menu> */}
-      <Link href="/sponsors" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          СПОНСОРИ
-        </Button>
-      </Link>
-      <Menu allowHover>
-        <MenuHandler>
-          <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-            ИСТОРИЯ
-          </Button>
-        </MenuHandler>
-        <MenuList>
-          <Link href="/history" passHref>
-            <MenuItem>2024</MenuItem>
-          </Link>
-        </MenuList>
-      </Menu>
-      <Link href="/contact" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          КОНТАКТИ
-        </Button>
-      </Link>
-    </ul>
-  );
-
-  const navListWithoutLogo = (
-    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Link href="/sign-up" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          РЕГИСТРАЦИЯ
-        </Button>
-      </Link>
-
-      <Menu allowHover>
-        <MenuHandler>
-          <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-            ТРАСЕТА
-          </Button>
-        </MenuHandler>
-        <MenuList>
-          <Link href="/track1" passHref>
-            <MenuItem>21 KM</MenuItem>
-          </Link>
-          <Link href="/track2" passHref>
-            <MenuItem>42 KM</MenuItem>
-          </Link>
-        </MenuList>
-      </Menu>
-
-      <Link href="/news" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          НОВИНИ
-        </Button>
-      </Link>
-
-      <Link href="/sponsors" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          СПОНСОРИ
-        </Button>
-      </Link>
-      <Menu allowHover>
-        <MenuHandler>
-          <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-            ИСТОРИЯ
-          </Button>
-        </MenuHandler>
-        <MenuList>
-          <Link href="/history" passHref>
-            <MenuItem>2024</MenuItem>
-          </Link>
-        </MenuList>
-      </Menu>
-      <Link href="/contact" passHref>
-        <Button variant="gradient" className="navbarButton lg:inline-block" color="green">
-          КОНТАКТИ
-        </Button>
-      </Link>
-    </ul>
-  );
   return (
     <div className="container absolute left-2/4 z-10 mx-auto -translate-x-2/4 p-4">
 
-      <div className="hidden lg:block">{navList}</div>
+      <div className="hidden lg:block">
+        <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+          {myList}
+        </ul>
+      </div>
       <IconButton
         variant="text"
         className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
@@ -193,7 +128,9 @@ export function Navbar() {
 
       <MobileNav open={openNav}>
         <div className="container mx-auto">
-          {navListWithoutLogo}
+          <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+            {myList}
+          </ul>
         </div>
       </MobileNav>
     </div>
