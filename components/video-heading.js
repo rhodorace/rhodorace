d1e85243921713cd45180266f4af6802b690d68d
rhodorace <rhodorace@gmail.com>
2023-10-05 18:53:24 +0200

    Video update

--- components/video-heading.js
+++ components/video-heading.js
@@ -16,7 +16,7 @@ export default function VideoHeading() {
             <div className="relative flex h-screen content-center items-center justify-center">
                 <div className="absolute top-0 h-full w-full  bg-cover bg-center" />
                 <video id="cbb795d9-1a70-23c9-0010-fccc2a385aa6-video" autoPlay loop muted playsInline data-wf-ignore="true" data-object-fit="cover" >
-                    <source src="/videos/intro-video.mp4" data-wf-ignore="true" />
+                    <source src="/videos/intro.mp4" data-wf-ignore="true" />
                 </video>
                 <Stripes />
             </div>
