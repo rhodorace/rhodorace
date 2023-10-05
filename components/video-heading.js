fd476a0ac395188f86ecf54233be91e8553455e0
rhodorace <rhodorace@gmail.com>
2023-10-05 19:21:27 +0200

    Refactor

--- components/video-heading.js
+++ components/video-heading.js
@@ -2,20 +2,11 @@ import Stripes from "./stripes";
 import React from "react";
 
 export default function VideoHeading() {
-    const [openNav, setOpenNav] = React.useState(false);
-
-    React.useEffect(() => {
-        window.addEventListener(
-            "resize",
-            () => window.innerWidth <= 960 && setOpenNav(true),
-        );
-    }, []);
-
     return (
         <>
             <div className="relative flex h-screen content-center items-center justify-center">
                 <div className="absolute top-0 h-full w-full  bg-cover bg-center" />
-                <video id="cbb795d9-1a70-23c9-0010-fccc2a385aa6-video" autoPlay loop muted playsInline data-wf-ignore="true" data-object-fit="cover" >
+                <video autoPlay loop muted playsInline data-wf-ignore="true" data-object-fit="cover" >
                     <source src="/videos/intro.mp4" data-wf-ignore="true" />
                 </video>
                 <Stripes />
