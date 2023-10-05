41963251f2601843b4bcb1284a28b34ab346aab5
rhodorace <rhodorace@gmail.com>
2023-10-05 18:57:33 +0200

    Web analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,13 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 import { appWithTranslation } from 'next-i18next'
+import { Analytics } from '@vercel/analytics/react';
 
 const App = ({ Component, pageProps }) => (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
 )
 
 export default appWithTranslation(App)
