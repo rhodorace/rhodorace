666f18c38018e1892d99c447cb5956f4541590eb
rhodorace <rhodorace@gmail.com>
2023-10-08 20:26:47 +0200

    Volunteer section

--- pages/volunteers.js
+++ pages/volunteers.js
@@ -2,37 +2,53 @@ import Footer from "@/components/footer";
 import { Navbar } from "@/components/navbar";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
-
+import {
+  Card,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
+import Image from "next/image";
 export default function Volunteers() {
   const { t } = useTranslation();
 
   return (
     <>
       <section className="relative block h-[50vh]">
-      <Navbar/>
+        <Navbar />
         <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/images/mountain.jpg')] bg-cover bg-center" />
         <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
       </section>
       <section className="relative bg-blue-gray-50/50 py-16 px-4">
         <div className="container mx-auto">
           <div className="relative mb-6 -mt-64 flex w-full min-w-0 flex-col break-words rounded-3xl bg-white shadow-xl shadow-gray-500/5">
-            <div className="px-6">
-              <div className="flex flex-wrap justify-center">
-                <div className="flex w-full justify-center px-4 lg:order-2 lg:w-3/12">
-                  <div className="relative">
-                    <div className="-mt-20 w-40">
-                    </div>
-                  </div>
-                </div>
-                <div className="w-full px-4 lg:order-1 lg:w-4/12">
-                  <div className="flex justify-center py-4 pt-8 lg:pt-4">
-                    Volunteers placeholder     
-                    </div>
-                  </div>
-                </div>
+            <div className="mt-14 flex flex-wrap items-center">
+              <div className="mx-auto -mt-8 w-full px-4 md:w-5/12">
+                <Typography
+                  variant="h3"
+                  className="mb-3 font-bold"
+                  color="blue-gray"
+                >
+                  {t("VOLUNTEERS_HEADER")}
+                </Typography>
+                <Typography className="mb-8 font-normal text-blue-gray-500">
+                  {t("VOLUNTEERS_BODY")}
+                </Typography>
+
+              </div>
+              <div className="mx-auto mt-24 flex w-full justify-center px-4 md:w-4/12 lg:mt-0">
+                <Card className="shadow-lg shadow-gray-500/10">
+                  <Image
+                    alt="Card Image"
+                    src="/images/rekke.jpg"
+                    className="rounded-2xl"
+                    width={600}
+                    height={100}
+                  />
+                </Card>
               </div>
             </div>
           </div>
+        </div>
       </section>
       <div className="bg-blue-gray-50/50">
         <Footer />
@@ -45,8 +61,8 @@ export async function getStaticProps(context) {
   const { locale } = context
 
   return {
-      props: {
-          ...(await serverSideTranslations(locale)),
-      },
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
   }
 }
