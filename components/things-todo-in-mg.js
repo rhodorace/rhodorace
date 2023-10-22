a4bbd1ce4408a31dcfb73b5f56998b36c50d2f95
rhodorace <rhodorace@gmail.com>
2023-10-22 11:30:35 +0200

    Translations

--- components/things-todo-in-mg.js
+++ components/things-todo-in-mg.js
@@ -101,7 +101,7 @@ export default function ThingsToDoInMg() {
 
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Харман кая</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("HARMAN_KAYA")}</h2>
                 <div class="flex items-center">
                     <div class="w-1/2 flex items-center justify-center">
                         <Image
@@ -114,9 +114,7 @@ export default function ThingsToDoInMg() {
                     </div>
                     <div class="w-1/2">
                         <Typography>
-                            Харман кая е едно от най-мащабните скално-изсечени праисторически култови комплекси в Родопите, намиращо се в землището на село Биволяне.
-                            Скално изсеченият археологически обект“ е разположен в едноименната местност, отстояща на половин километър северно и североизточно от махалите Абица и Гъсак на село Биволяне.
-                            Мегалитният комплекс се е развил около малка естествена пещера.
+                            {t("HARMAN_KAYA_INFO")}
                         </Typography>
                     </div>
                 </div>
@@ -136,14 +134,11 @@ export default function ThingsToDoInMg() {
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Религиозен комплекс „Елмалъ баба текке"</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("ELMALI_BABA")}</h2>
                 <div class="flex items-center">
                     <div class="w-1/2">
                         <Typography>
-                            „Елмалъ баба текке" е исторически обособен и действащ религиозен комплекс, състоящ се от тюрбе, джамия, обреден дом и прилежащи сгради.
-                            Намира се в близост до с. Биволяне и Харман кая. В теккето ежегодно се организира най-големият събор в региона, посещаван масово от туристи от страната и чужбина.
-                            Тюрбето няма аналог в България – в него са разположени 7 безименни гроба.
-                            В едната част са погребани шестима мюсюлмански светци, а в другата е гробът на много почитаната майка Фатима.
+                            {t("ELMALI_BABA_INFO")}
                         </Typography>
                     </div>
                     <div class="w-1/2 flex items-center justify-center">
@@ -158,7 +153,7 @@ export default function ThingsToDoInMg() {
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Лесопарк „Момчил юнак“</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("LESOPARK")}</h2>
                 <div class="flex items-center">
                     <div className="w-1/2 flex items-center justify-center">
                         <Image
@@ -171,23 +166,17 @@ export default function ThingsToDoInMg() {
                     </div>
                     <div className="w-1/2">
                         <Typography>
-                            Лесопарк "Момчил юнак" е обичана и посещавана местност. Намира се над Момчилград и открива прекрасен пейзаж към цялата околност.
-                            Има изградена инфраструктура и прекрасни условия за отдих, спорт и почивка.
-                            Наблизо е построен и параклис "Св. Дух", около който ежегодно се провеждат общоградски събори.
+                            {t("LESOPARK_INFO")}
                         </Typography>
                     </div>
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Водните лилии</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("VODNI_LILI")}</h2>
                 <div class="flex items-center">
                     <div className="w-1/2">
                         <Typography>
-                            Разположено е в землището на с. Летовник, в региона на Община Момчилград и е единственото в област Кърджали естествено находище на водни лилии.
-                            Те цъфтят в края на юни и началото на юли и заемат почти цялата водна повърхност на езерото.
-                            Гледката е изключително красива и впечатлява всеки.
-                            Никой не помни кога за първи път в езерото са се появили лилиите.
-                            Уникално красиво езеро, чиято повърхност е покрита с хиляди водни лилии. В света има само 13 такива езера.
+                            {t("VODNI_LILI_INFO")}
                         </Typography>
                     </div>
                     <div className="w-1/2 flex items-center justify-center">
@@ -202,12 +191,9 @@ export default function ThingsToDoInMg() {
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Скални ниши, с. Неофит Бозвелиево</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("SKALNI_NISHI")}</h2>
                 <Typography>
-                    Нишите са разположени на открояваща се скала вдясно от пътя в посока към с. Лале.
-                    Издълбани са на достъпно място, във варовик, което се среща сравнително рядко. Те са 11 на брой, разпределени на 2 нива.
-                    Сред тях има и недовършени, което дава възможност да се проследи начинът на изсичане.
-                    На хълма непосредствено над скалите с нишите има разрушено светилище, районът е осеян с керамика.
+                    {t("SKALNI_NISHI_INFO")}
                 </Typography>
                 <div className="flex space-x-4 mt-5">
                     <Image
@@ -227,7 +213,7 @@ export default function ThingsToDoInMg() {
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Дивечевъдно стопанство „Студен кладенец“</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("STUDEN_KLADENEC")}</h2>
                 <div class="flex items-center">
                     <div class="w-1/2 flex items-center justify-center">
                         <Image
@@ -240,11 +226,7 @@ export default function ThingsToDoInMg() {
                     </div>
                     <div class="w-1/2">
                         <Typography>
-                            Заема площ от 5186 хектара сред изключително красив ландшафт, в района на вливането на река Нановишка в яз. Студен кладенец.
-                            Необезпокоявани от хората там обитават елени-лопатари, муфлони, диви свине, сърни, кози, зайци, лисици, вълци, кеклик, яребици и др., а във водите се срещат мряна, скобар, шаран, сом и др.
-                            Дивечевъдното стопанство основно се занимава с провеждане на организиран ловен туризъм. В рамките на стопанството се намира скалния масив „Юмрук кая", където гнездят белоглав, черен и египетски лешояд и скални орли, които са защитени в цяла Европа.
-                            Убежище намират 219 вида птици, от които 59 са в Червената книга на България и 69 са включени като защитени по „Натура 2000". Тук се среща и дивият червен божур.
-                            Стопанството разполага с две хижи – една обновена и една малка хижа на брега на язовира и е предпочитано място за отдих, лов и риболов.
+                            {t("STUDEN_KLADENEC_INFO")}
                         </Typography>
                     </div>
                 </div>
@@ -270,7 +252,7 @@ export default function ThingsToDoInMg() {
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Църквата „Свети цар Борис I Покръстител“</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("CHURCH")}</h2>
                 <div class="flex items-center">
                     <div class="w-1/2 flex items-center justify-center">
                         <Image
@@ -283,22 +265,17 @@ export default function ThingsToDoInMg() {
                     </div>
                     <div class="w-1/2">
                         <Typography>
-                            „Свети цар Борис I Покръстител“ е православен храм, който се намира в центъра на Момчилград.
-                            Църквата датира от 1938 – 1939 г. и е построена на мястото на малък дървен параклис.
-                            Като паметник на културата, храмът е основно реставриран и е осветен след 3-годишен ремонт на храмовия празник 2 май през 2009 г.
-                            Църквата е прочута с уникалната си дърворезба на иконостаса и владишкия престол.
+                            {t("CHURCH_INFO")}
                         </Typography>
                     </div>
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Старата градска джамия</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("MOSQUE")}</h2>
                 <div class="flex items-center">
                     <div class="w-1/2">
                         <Typography>
-                            Старата джамия е построена преди около 300 години. От самото си изграждане до днес винаги е била действащ религиозен храм.
-                            Уникална е с това, че e единствената джамия в област Кърджали с две минарета. През 2004г е извършена основна реставрация на храма.
-                            Намира се от дясната страна на пътя, пресичащ централната част на град Момчилград, в непосредствена близост до РУ "Полиция" и Горското стопанство.
+                            {t("MOSQUE_INFO")}
                         </Typography>
                     </div>
                     <div class="w-1/2 flex items-center justify-center">
@@ -313,10 +290,9 @@ export default function ThingsToDoInMg() {
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
-                <h2 class="mb-12 text-center text-3xl font-bold">Спортни съоръжения</h2>
+                <h2 class="mb-12 text-center text-3xl font-bold">{t("SPORTS")}</h2>
                 <Typography>
-                    Момчилград разполага с един от най-добрите спортни бази в Южна България – стадион „Хасан Семерджи“ и спортен комплекс Наим Сюлейманоглу.
-                    Тук тренират отборите на известни спортни клубове, сред които участникът в Първа лига Арда, както и водещи тенис клубове в страната.
+                    {t("SPORTS_INFO")}
                 </Typography>
                 <div className="flex space-x-4 mt-5">
                     <Image
