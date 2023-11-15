6a4a8d5f6e3ad1566291656ed0beb4ef0be40a3d
rhodorace <rhodorace@gmail.com>
2023-11-15 20:47:47 +0100

    Compress mountain jpg

--- components/container.js
+++ components/container.js
@@ -6,7 +6,7 @@ export default function Container({ content }) {
         <>
             <section className="relative block h-[50vh]">
                 <Navbar />
-                <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/images/mountain.jpg')] bg-cover bg-center" />
+                <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/images/background1.jpg')] bg-cover bg-center" />
                 <div className="absolute top-0 h-full w-full bg-black/75 bg-cover bg-center" />
             </section>
             <section className="relative bg-blue-gray-50/50 py-16 px-4">
