14c851093a0ff154d1423dac8bd390f69f9ad433
rhodorace <rhodorace@gmail.com>
2023-11-15 20:56:27 +0100

    Compress video

--- components/video-heading.js
+++ components/video-heading.js
@@ -7,7 +7,7 @@ export default function VideoHeading() {
             <div className="relative flex h-screen content-center items-center justify-center">
                 <div className="absolute top-0 h-full w-full  bg-cover bg-center" />
                 <video autoPlay loop muted playsInline data-wf-ignore="true" data-object-fit="cover" >
-                    <source src="/videos/intro.mp4" data-wf-ignore="true" />
+                    <source src="/videos/testmovie.mp4" data-wf-ignore="true" />
                 </video>
                 <Stripes />
             </div>
