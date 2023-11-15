1c0bd83f081c01ce6ec6f7070b642ff3fc0491e3
rhodorace <rhodorace@gmail.com>
2023-11-15 20:42:38 +0100

    Adjust sign up

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -9,11 +9,50 @@ import {
   AccordionBody,
 } from "@material-tailwind/react";
 import { Card, Typography } from "@material-tailwind/react";
-import Link from 'next/link';
-import { Button } from "@material-tailwind/react";
+import { useEffect } from 'react';
+import { useRouter } from "next/router";
 
 export default function SignUp() {
   const { t } = useTranslation();
+  const router = useRouter();
+
+  useEffect(() => {
+    var linkIrun;
+    if (router.locale === "en") {
+      linkIrun = "https://www.irun.bg/external.php/competition/sign/name/rhodo-rock-2024?s_lang=en_GB";
+    } else {
+      linkIrun = "https://www.irun.bg/external.php/competition/sign/name/rhodo-rock-2024?s_lang=bg_BG";
+    }
+
+    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    var ua = navigator.userAgent.toLowerCase();
+    var supportedExternalIrun = false;
+    if (isSafari) {
+      //Safari
+      document.getElementById("irun_register").style.display = "flex";
+      document.getElementById("irun_link").href = linkIrun;
+    } else {
+      var receiveMessage = function (evt) {
+        if (evt.data === 'MM:3PCunsupported') {
+          document.getElementById("irun_register").style.display = "flex";
+          document.getElementById("irun_link").href = linkIrun;
+        } else if (evt.data === 'MM:3PCsupported') {
+          supportedExternalIrun = true;
+          document.getElementById("irun_iframe").src = linkIrun;
+          document.getElementById("irun_iframe").style.display = "block";
+          document.getElementById("irun_link2").href = linkIrun;
+        }
+      };
+      window.addEventListener("message", receiveMessage, false);
+      setTimeout(function () {
+        if (!supportedExternalIrun) {
+          document.getElementById("irun_register").style.display = "flex";
+          document.getElementById("irun_link").href = linkIrun;
+        }
+      }, 5000);
+    }
+  });
+
   const TABLE_HEAD = [t("FROM"), t("TILL"), "22KM", "50KM"];
 
   const TABLE_ROWS = [
@@ -116,10 +155,21 @@ export default function SignUp() {
                     </table>
                   </Card>
                   <div className='mt-5'>
-                    <Link href="/sign-up-irun" passHref>
-                      <Button variant="outlined">{t("SIGN_UP")}</Button>
-                    </Link>
-                  </div>
+                    
+                      <div id="irun_register">
+                        <div style={{ width: "80%", textAlign: "center" }}>Вашият браузър е ограничил функционалността за записване. За да продължите натиснете бутона по-долу и ще ви се
+                          зареди регистрационната форма в нов прозорец, където ще можете да завършите процеса.
+                          <a id="irun_link" href="#" target="_blank">Регистрация (натиснете тук)</a>
+                        </div>
+                      </div>
+                      <iframe id="irun_iframe" style={{ display: "none" }} src="" width="100%" height="900px" frameborder="0">
+                        Вашият браузър е ограничил функционалността за записване. За да продължите натиснете линка по-долу и ще ви се
+                        зареди регистрационната форма в нов прозорец, където ще можете да завършите процеса.
+                        <a href="#" id="irun_link2" target="_blank">Регистрация (натиснете тук)</a>
+                      </iframe>
+                      <iframe src="https://www.irun.bg/cookies/start.html" style={{ display: "none" }}></iframe>
+                    </div>
+                  
                 </AccordionBody>
               </Accordion>
             </>
