3e777714263fcad00b843ca92f7513ed2cef23c3
rhodorace <rhodorace@gmail.com>
2023-11-20 22:09:28 +0100

    Align text

--- components/things-todo-in-mg.js
+++ components/things-todo-in-mg.js
@@ -58,98 +58,77 @@ export default function ThingsToDoInMg() {
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("PETRIFIED_FOREST")}</h2>
-                <div class="flex items-center">
-                    <div class="w-1/2">
-                         <Typography>
-                            {t("PETRIFIED_FOREST_INFO")}
-                        </Typography>
-                    </div>
-                    <div class="w-1/2 flex items-center justify-center">
+                <Typography>
+                    {t("PETRIFIED_FOREST_INFO")}
+                </Typography>
+                <div class="flex items-center justify-center mt-8">
                     <Image
-                            alt="Athlets"
-                            src="/images/vkamenenagora.png"
-                            className="rounded-xl"
-                            width={300}
-                            height={100}
-                        />
-                    </div>
+                        alt="Athlets"
+                        src="/images/vkamenenagora.png"
+                        className="rounded-xl"
+                        width={300}
+                        height={100}
+                    />
                 </div>
-
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("HARMAN_KAYA")}</h2>
-                <div class="flex items-center">
-                    <div className="w-1/2 flex items-center justify-center">
-                        <Image
-                            alt="Athlets"
-                            src="/images/harman_kaya.jpg"
-                            className="rounded-xl"
-                            width={400}
-                            height={100}
-                        />
-                    </div>
-                    <div className="w-1/2">
-                        <Typography>
-                            {t("HARMAN_KAYA_INFO")}
-                        </Typography>
-                    </div>
+                <Typography>
+                    {t("HARMAN_KAYA_INFO")}
+                </Typography>
+                <div className="flex items-center justify-center mt-8">
+                    <Image
+                        alt="Athlets"
+                        src="/images/harman_kaya.jpg"
+                        className="rounded-xl"
+                        width={400}
+                        height={100}
+                    />
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("ELMALI_BABA")}</h2>
-                <div class="flex items-center">
-                    <div class="w-1/2">
-                        <Typography>
-                            {t("ELMALI_BABA_INFO")}
-                        </Typography>
-                    </div>
-                    <div class="w-1/2 flex items-center justify-center">
-                        <Image
-                            alt="Athlets"
-                            src="/images/elmalibaba.jpg"
-                            className="rounded-xl"
-                            width={400}
-                            height={100}
-                        />
-                    </div>
+                <Typography>
+                    {t("ELMALI_BABA_INFO")}
+                </Typography>
+                <div class="flex items-center justify-center mt-8">
+                    <Image
+                        alt="Athlets"
+                        src="/images/elmalibaba.jpg"
+                        className="rounded-xl"
+                        width={400}
+                        height={100}
+                    />
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("LESOPARK")}</h2>
-                <div class="flex items-center">
-                    <div className="w-1/2 flex items-center justify-center">
-                        <Image
-                            alt="Athlets"
-                            src="/images/lesopark_mg.jpg"
-                            className="rounded-xl"
-                            width={400}
-                            height={100}
-                        />
-                    </div>
-                    <div className="w-1/2">
-                        <Typography>
-                            {t("LESOPARK_INFO")}
-                        </Typography>
-                    </div>
+                <Typography>
+                    {t("LESOPARK_INFO")}
+                </Typography>
+                <div className="flex items-center justify-center mt-8">
+                    <Image
+                        alt="Athlets"
+                        src="/images/lesopark_mg.jpg"
+                        className="rounded-xl"
+                        width={400}
+                        height={100}
+                    />
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("VODNI_LILI")}</h2>
-                <div class="flex items-center">
-                    <div className="w-1/2">
-                        <Typography>
-                            {t("VODNI_LILI_INFO")}
-                        </Typography>
-                    </div>
-                    <div className="w-1/2 flex items-center justify-center">
-                        <Image
-                            alt="Athlets"
-                            src="/images/vodni_lili.jpg"
-                            className="rounded-xl"
-                            width={400}
-                            height={100}
-                        />
-                    </div>
+                <Typography>
+                    {t("VODNI_LILI_INFO")}
+                </Typography>
+                <div className="flex items-center justify-center mt-8">
+                    <Image
+                        alt="Athlets"
+                        src="/images/vodni_lili.jpg"
+                        className="rounded-xl"
+                        width={400}
+                        height={100}
+                    />
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
@@ -176,21 +155,17 @@ export default function ThingsToDoInMg() {
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("STUDEN_KLADENEC")}</h2>
-                <div class="flex items-center">
-                    <div class="w-1/2 flex items-center justify-center">
-                        <Image
-                            alt="Athlets"
-                            src="/images/studen1.jpg"
-                            className="rounded-xl"
-                            width={400}
-                            height={100}
-                        />
-                    </div>
-                    <div class="w-1/2">
-                        <Typography>
-                            {t("STUDEN_KLADENEC_INFO")}
-                        </Typography>
-                    </div>
+                <Typography>
+                    {t("STUDEN_KLADENEC_INFO")}
+                </Typography>
+                <div class="flex items-center justify-center mt-8">
+                    <Image
+                        alt="Athlets"
+                        src="/images/studen1.jpg"
+                        className="rounded-xl"
+                        width={400}
+                        height={100}
+                    />
                 </div>
                 <div class="flex items-center mt-5">
                     <div class="w-1/2 flex items-center justify-center">
@@ -215,40 +190,33 @@ export default function ThingsToDoInMg() {
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("CHURCH")}</h2>
-                <div class="flex items-center">
-                    <div class="w-1/2 flex items-center justify-center">
-                        <Image
-                            alt="Athlets"
-                            src="/images/church.jpg"
-                            className="rounded-xl"
-                            width={400}
-                            height={100}
-                        />
-                    </div>
-                    <div class="w-1/2">
-                        <Typography>
-                            {t("CHURCH_INFO")}
-                        </Typography>
-                    </div>
+                <Typography>
+                    {t("CHURCH_INFO")}
+                </Typography>
+                <div class="flex items-center justify-center mt-8">
+                    <Image
+                        alt="Athlets"
+                        src="/images/church.jpg"
+                        className="rounded-xl"
+                        width={400}
+                        height={100}
+                    />
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
                 <h2 class="mb-12 text-center text-3xl font-bold">{t("MOSQUE")}</h2>
-                <div class="flex items-center">
-                    <div class="w-1/2">
-                        <Typography>
-                            {t("MOSQUE_INFO")}
-                        </Typography>
-                    </div>
-                    <div class="w-1/2 flex items-center justify-center">
-                        <Image
-                            alt="Mosque"
-                            src="/images/mosque.jpg"
-                            className="rounded-xl"
-                            width={400}
-                            height={100}
-                        />
-                    </div>
+                <Typography>
+                    {t("MOSQUE_INFO")}
+                </Typography>
+
+                <div class="flex items-center justify-center mt-8">
+                    <Image
+                        alt="Mosque"
+                        src="/images/mosque.jpg"
+                        className="rounded-xl"
+                        width={400}
+                        height={100}
+                    />
                 </div>
             </section>
             <section class="mb-16 text-center md:text-left">
